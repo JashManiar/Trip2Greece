4d17900f1aac6d62cea7aec13d90276d41acd23c
Jash Maniar <101001262+JashManiar@users.noreply.github.com>
2022-10-11 18:47:56 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+var synth = window.speechSynthesis;
+speak_data = "Welcome to my first independent website. Its going to be about my pre trip preparations and post trip experience. I am sure this will be very helpful on your trip to greece. If you're wondering how to plan your first trip to Greece then this Greece travel guide, packed with Greece travel tips, is for you. Everything from where to go in Greece, things to do in Greece, how to travel Greece, Greek island hopping, Greek food and much more."
+"In this Greek Islands travel guide, I'll cover Athens, Santorini, Paros, Antiparos, Rhodos, Crete, Mykonos and other greek islands. If you plan on doing Greece travel, in this Greece travel vlog, you'll learn everything about how to plan a trip to Greece - packing for Greece, how to travel around Greece, Greek style, how to see Greece, Greek ferries, best time to visit Greece, best places to visit in Greece, what to eat in Greece and so much more." 
+"Traveling to Greece and travel in Greece will be easy with these travel tips. Enjoy your trip to Greece and vacation in Greece! Greece is truly a one-of-a-kind country!";
+var utterThis = new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
 
+function enter_web()
+{
+    window.location = "page2.html";
+}
 
-    var synth = window.speechSynthesis;
-    speak_data = "Welcome to my first independent website. Its going to be about my pre trip preparations and post trip experience. I am sure this will be very helpful on your trip to greece. If you're wondering how to plan your first trip to Greece then this Greece travel guide, packed with Greece travel tips, is for you. Everything from where to go in Greece, things to do in Greece, how to travel Greece, Greek island hopping, Greek food and much more."
-    "In this Greek Islands travel guide, I'll cover Athens, Santorini, Paros, Antiparos, Rhodos, Crete, Mykonos and other greek islands. If you plan on doing Greece travel, in this Greece travel vlog, you'll learn everything about how to plan a trip to Greece - packing for Greece, how to travel around Greece, Greek style, how to see Greece, Greek ferries, best time to visit Greece, best places to visit in Greece, what to eat in Greece and so much more." 
-    "Traveling to Greece and travel in Greece will be easy with these travel tips. Enjoy your trip to Greece and vacation in Greece! Greece is truly a one-of-a-kind country!";
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-    synth.speak(utterThis);
 
 
